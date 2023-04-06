9e7faa17e5536421d00c3814f05f6d2d317354a8
Parul G <pg7wfm@virginia.edu>
2023-04-06 16:11:44 -0400

    added lib event to nav bar

--- lib-event.php
+++ lib-event.php
@@ -5,6 +5,7 @@
   $reviews = getReviewsForBook($_POST['book_to_view'])
 ?>
 
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -34,44 +35,6 @@
       <li class="nav-item">
         <a class="nav-link" href="profile.php">Profile</a>
       </li>
-      
     </ul>
   </div>
 </nav>
-<br>
-    <h1> <?php echo $thisbook['title'];?> </h1>
-    <p>
-      Title: <?php echo $thisbook['title']?>
-      <br>
-      Author: <?php echo $thisbook['author']?>.
-      <br>
-      Published Date: <?php echo $thisbook['date_published']?>
-      <br>
-      ISBN: <?php echo $thisbook['isbn']?>
-      <br>
-      Copies Available: <?php echo $thisbook['total_copies'] - $thisbook['copies_checked_out']?>
-      <br>
-    </p>
-    <div class="row justify-content-center">  
-      <table class="w3-table w3-bordered w3-card-4 center" style="width:70%">
-        <thead>
-          <tr style="background-color:#B0B0B0">
-            <th>Author</th>
-            <th>Title</th>
-            <th>Review Content</th>
-          </tr>
-        </thead>
-        <?php foreach ($reviews as $item): ?>
-          <tr>
-            <td><?php echo $item['username']; ?></td>
-            <td><?php echo $item['title']; ?></td>  
-            <td><?php echo $item['body']; ?></td>           
-          </tr>
-        <?php endforeach; ?>
-      </table>
-    </div>
-
-
-
-  </body>
-</html>

3e3d568c43cd3f128b74eed7cb61a50b57ecb1ca
Parul G <pg7wfm@virginia.edu>
2023-04-29 16:53:03 -0400

    adding have read button

--- bookinfo.php
+++ bookinfo.php
@@ -102,6 +102,15 @@
           </div>
         </form>
     </div>
+    <div>
+        <form name="haveRead" action= "profile.php" method="post">
+          <div class="row mb-3 mx-3">
+            <input type="submit" class="btn btn-primary" name="actionBtn" value="Have Read" title="Have Read" />
+            <input type="hidden" name="book_to_haveread" value="<?php echo $thisbook['isbn']; ?>"/>
+            <input type="hidden" name="user_checking_out_haveread" value="<?php echo $_SESSION["userN"]; ?>"/>        
+          </div>
+        </form>
+    </div>
     <div class="row justify-content-center">  
       <table class="w3-table w3-bordered w3-card-4 center" style="width:70%">
         <thead>

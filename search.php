c56c8069dd70a51baa2c9cd9bfd94618ff125399
TI-Allard <allard.ti@gmail.com>
2023-04-30 16:23:20 -0400

    fixed seeing remove button and column when not admin

--- search.php
+++ search.php
@@ -51,7 +51,7 @@
   <tr style="background-color:#B0B0B0">
     <th>Title</th>
     <th>Book Information</th>
-    <?php if($admin_logged_in == TRUE): ?>
+    <?php if($admin_logged_in[0] == TRUE): ?>
       <th>Remove Book?</th>
     <?php endif; ?>
   </tr>
@@ -65,7 +65,7 @@
          <input type="hidden" name="book_to_view" value="<?php echo $item['isbn']; ?>"/>
        </form>
      </td>     
-     <?php if($admin_logged_in == TRUE): ?>
+     <?php if($admin_logged_in[0] == TRUE): ?>
       <td>
         <form action="home.php" method="post">
           <input type="submit" class="btn btn-danger" name="actionBtn" value="Delete"/>
